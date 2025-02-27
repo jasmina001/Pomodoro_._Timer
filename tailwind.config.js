c99e0529414ea443a27a0dfe06975368f6d6bdf4
jasmina001 <jorayevajasmina76@gmail.com>
2025-02-27 21:08:28 +0500

    add

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}"], // <-- src ham qo‘shildi!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
